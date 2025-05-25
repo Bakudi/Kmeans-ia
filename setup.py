59a97131cca701ec5aa06505ac50b0e7d9a3b522
Andres Felipe Rojas <107456253+Bakudi@users.noreply.github.com>
2025-05-25 15:16:41 -0500

    carpeta eliminada

---
+++ setup.py
@@ -0,0 +1,5 @@
+"""
+Archivo de empaquetado en Python
+Este archivo es el encargado de empaquetar el proyecto en un paquete instalable.
+El archivo setup.py es un script de configuración que se utiliza para empaquetar y distribuir proyectos de Python.
+"""
